8065939a814b502c258c5501c5722552af716f72
jeki91 <esmet@mail.ua>
2020-10-26 12:21:11 +0300

    new

---
+++ js/jakor.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function () {
+
+	$('.nav a[href^="#"]').click(function () {
+		//Сохраняем значение атрибута href в переменной:
+		var offset = $('.header').innerHeight();
+		var target = $(this).attr('href');
+		$('html, body').animate({
+			scrollTop: $(target).offset().top - offset//можно вычесть высоту меню
+		}, 500);
+	});
+
+});
